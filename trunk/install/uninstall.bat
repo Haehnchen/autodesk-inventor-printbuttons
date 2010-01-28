21caaaf82e31076581a5a7b6ae4f15251769941e
espendiller <espendiller@83ba0bc4-b367-4447-90b9-38eaa09dd1ce>
2010-01-28 06:55:20 +0000

    relative path for batchfiles

--- trunk/install/uninstall.bat
+++ trunk/install/uninstall.bat
@@ -8,12 +8,12 @@ goto end
 
 :x86
 echo found x86 system  
-%windir%\Microsoft.NET\Framework\v2.0.50727\RegAsm.exe /unregister PrintButtons.dll
+%windir%\Microsoft.NET\Framework\v2.0.50727\RegAsm.exe /unregister ../PrintButtons.dll
 goto end
 
 :x64
 echo found x64 system  
-%windir%\Microsoft.NET\Framework64\v2.0.50727\RegAsm.exe /unregister PrintButtons.dll
+%windir%\Microsoft.NET\Framework64\v2.0.50727\RegAsm.exe /unregister ../PrintButtons.dll
 goto end
 
 
